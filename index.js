f2ed40fca1bdc9e08797c45c03d005280a0802f3
Maximiliano Cortez <mcortez@koandina.com>
2020-03-19 16:17:09 -0300

    Solucionado eliminacion de archivo PDF original luego de ser procesado correctamente

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 var fileupload = require("express-fileupload");
 const fs = require('fs');
 const pdf = require('pdf-parse');
+const path = require("path");
 
 var pathFile = "";
 app.use(fileupload());
@@ -11,40 +12,61 @@ app.use(fileupload());
 app.set("port", process.env.PORT || "3000");
 
 app.get("/", (req, res, next)=> {
-    res.status(200).send("Control de Existencias - Andina Argentina.");
-})
+    res.status(200).json({ 
+        "Respuesta": "Control de Existencias - Andina Argentina", 
+        "Autor": "Maximiliano Cortez"
+    });
+});
 
 app.get("/api", (req, res, next)=> {
-    res.status(200).send("Control de Existencias - Andina Argentina, utilice POS");
-})
+    res.status(200).json({ 
+        "Respuesta": "APIS - Control de Existencias - Andina Argentina", 
+        "Autor": "Maximiliano Cortez"
+    });
+});
 
-app.post("/api/upload", (req, res, next) => {
-    const file = req.files.pdfupdated;
-    pathFile = "./upload/" + file.name
-    file.mv(pathFile, (err, result)=> {
-        if (err) {
-            throw err;
-        }
-        
-         readPdf(pathFile, res);
-        
-        //res.send({
-        //    success: true,
-        //    message: "Archivo subido",
-        //    getreadpdf: resultado
-        //})
+app.get("/api/upload", (req, res, next)=> {
+    res.status(200).json({ 
+        "Respuesta": "API/UPLOAD PDF AND READ CAE - Control de Existencias - Andina Argentina", 
+        "Autor": "Maximiliano Cortez"
     });
 })
 
-app.listen(app.get("port"), ()=> {
-    console.log("Servidor funcionando en el puerto 3000");
-})
+app.post("/api/upload", (req, res, next) => {
+    try {
+        const file = req.files.pdfupdated;
+        pathFile = path.join(__dirname, "/upload/", file.name);
+        file.mv(pathFile, (err)=> {
+            if (err) {
+                throw err;
+            }
+            let dataBuffer = fs.readFileSync(pathFile);
+            pdf(dataBuffer).then((data)=> {
+                fs.unlink(pathFile, (err)=>{
+                    if (err) {
+                        throw err;
+                    }
+                })
+                res.status(200).json({ "Respuesta": data.text.split("\n"), 
+                    "File": "file.name", 
+                    "Autor": "Maximiliano Cortez"                    
+                });                
+            }).catch((err)=> {
+                res.status(403).json({ "Error": err, 
+                    "Autor": "Maximiliano Cortez"
+                });             
+            });
+        });    
+    } catch(err) {
+        res.status(400).json({ "Error": err, 
+            "Autor": "Maximiliano Cortez"
+        });             
+    }
+});
 
 function readPdf(pathDir, response){
-    //let dataBuffer = fs.readFileSync('path to PDF file...');
     let dataBuffer = fs.readFileSync(pathDir);
     pdf(dataBuffer).then((data)=> {
-    
         // number of pages
         // console.log(data.numpages);
         // number of rendered pages
@@ -57,17 +79,24 @@ function readPdf(pathDir, response){
         // check https://mozilla.github.io/pdf.js/getting_started/
         // console.log(data.version);
         // PDF text
-        response.send({
-            success: true,
-            message: "Archivo subido",
-            resultado: data.text.split("\n")
-        })
+
+        //response.send({
+        //    success: true,
+        //    message: "Archivo subido",
+        //    resultado: data.text.split("\n")
+        //})
+
+        response.send(data.text.split("\n")[12]);
             
     }).catch((err)=> {
         response.send({
             success: true,
             message: "Archivo subido",
             error: err
-        })
-    })
+        });
+    });
 };
+
+app.listen(app.get("port"), ()=> {
+    console.log("Servidor funcionando en el puerto 3000");
+});
